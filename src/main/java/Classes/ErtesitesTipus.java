eddc78388c083fadb01bd2c39551a2bfe0604351
bla255 <bla255@windowslive.com>
2024-11-12 22:43:10 +0100

    Javadoc kezdet 4 osztalyra

--- src/main/java/Classes/ErtesitesTipus.java
+++ src/main/java/Classes/ErtesitesTipus.java
@@ -1,8 +1,28 @@
 package Classes;
 
+/**
+ * Az ErtesitesTipus enum az értesítések lehetséges típusait határozza meg,
+ * amelyeket különböző csatornákon keresztül lehet továbbítani.
+ */
 public enum ErtesitesTipus {
+
+    /**
+     * E-mail alapú értesítés küldése.
+     */
     EMAIL,
+
+    /**
+     * Mobiltelefon alapú értesítés küldése, például SMS-ben.
+     */
     MOBIL,
+
+    /**
+     * Postai úton történő értesítés küldése.
+     */
     POSTA,
+
+    /**
+     * Push értesítés küldése egy eszközre, például egy mobilalkalmazáson keresztül.
+     */
     PUSH
 }
